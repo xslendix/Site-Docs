67ae940a9389dc7f6da28e9beed1fed47429ec14
xzntrc <96399844+xzntrc@users.noreply.github.com>
2022-05-04 21:49:48 +1000

    Changed Navbar Title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,7 +44,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'TempleOS Simplified',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
